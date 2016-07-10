c000787caf2ed252d146c316e40ed1146abc5c07
pmatushkin <pmatushkin@gmail.com>
2016-07-10 13:32:22 -0400

    feat: Pass Problem 3: Sorting

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 Apply the divide-and-conquer technique to solve various computational problems efficiently:
 * [`/src/binary_search/BinarySearch.java`](src/binary_search/BinarySearch.java): Problem 1, Binary Search, implements the binary search algorithm (PASSED);
 * [`/src/majority_element/MajorityElement.java`](src/majority_element/MajorityElement.java): Problem 2, Majority Element, checks whether an input sequence contains a majority element;
-* [`/src/sorting/Sorting.java`](src/sorting/Sorting.java): Problem 3, Sorting: 3-Way Partition, in the given implementation of the quick sort algorithm replaces a 2-way partition with a 3-way partition;
+* [`/src/sorting/Sorting.java`](src/sorting/Sorting.java): Problem 3, Sorting: 3-Way Partition, in the given implementation of the quick sort algorithm replaces a 2-way partition with a 3-way partition (PASSED);
 * [`/src/inversions/Inversions.java`](src/inversions/Inversions.java): Advanced Problem 4, Number of Inversions, counts the number of inversions of a given sequence;
 * [`/src/points_and_segments/PointsAndSegments.java`](src/points_and_segments/PointsAndSegments.java): Advanced Problem 5, Points and Segments, computes, for each point, the number of segments that contain this point.
--- src/sorting/Sorting.java
+++ src/sorting/Sorting.java
@@ -5,13 +5,57 @@ public class Sorting {
     private static Random random = new Random();
 
     private static int[] partition3(int[] a, int l, int r) {
-      //write your code here
+        //write your code here
 
+        // this is the pivot element
+        int x = a[l];
 
-      int m1 = l;
-      int m2 = r;
-      int[] m = {m1, m2};
-      return m;
+        // these are the indices of the subarray of the equal elements
+        // (all of these elements are equal to the pivot element)
+        int m1 = l;
+        int m2 = l;
+
+        for (int i = l + 1; i <= r; i++) {
+            int ai = a[i];
+            if (ai < x) {
+//                int t1 = a[m1];
+//                int t2 = a[m2 + 1];
+
+//                // the found lesser element switches places with the first element after the subarray of equals
+//                a[m2 + 1] = a[i];
+//                a[i] = t2;
+//                // the first element of the subarray of equals switches places
+//                // with the first element after the subarray of equals
+//                a[m1] = a[m2 + 1];
+//                a[m2 + 1] = t1;
+
+                // ... or just do this
+                a[m1++] = ai;
+                a[i] = a[++m2];
+                a[m2] = x;
+
+                // move the beginning and ending indices of the subarray of equals
+//                m1++;
+//                m2++;
+            } else if (ai > x) {
+
+            } else {
+//                // the found greater element switches places with the first element after the subarray of equals
+//                int t = a[m2 + 1];
+//                a[m2 + 1] = a[i];
+//                a[i] = t;
+//                m2++;
+
+                // ... or this
+                // the difference is 1.06/5.50 vs 1.11/5.50
+                int t = a[++m2];
+                a[m2] = ai;
+                a[i] = t;
+            }
+        }
+
+        int[] m = {m1, m2};
+        return m;
     }
 
     private static int partition2(int[] a, int l, int r) {
@@ -39,10 +83,14 @@ public class Sorting {
         int t = a[l];
         a[l] = a[k];
         a[k] = t;
+
         //use partition3
-        int m = partition2(a, l, r);
-        randomizedQuickSort(a, l, m - 1);
-        randomizedQuickSort(a, m + 1, r);
+//        int m = partition2(a, l, r);
+//        randomizedQuickSort(a, l, m - 1);
+//        randomizedQuickSort(a, m + 1, r);
+        int[] m = partition3(a, l, r);
+        randomizedQuickSort(a, l, m[0] - 1);
+        randomizedQuickSort(a, m[1] + 1, r);
     }
 
     public static void main(String[] args) {
@@ -52,10 +100,19 @@ public class Sorting {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
+
         randomizedQuickSort(a, 0, n - 1);
         for (int i = 0; i < n; i++) {
             System.out.print(a[i] + " ");
         }
+        System.out.println();
+    }
+
+    public static void printlnArray(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
     }
 
     static class FastScanner {
